89ed319eed0b1af46b26a62166f68da832c89dc2
Afan Ray Mahardika <36183665+afanray@users.noreply.github.com>
2023-10-27 20:38:51 +0700

    Update Struktur desa contoh

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/Screenshot 2023-10-20 at 10.56.39.png" alt="" />
+      <img src="@/assets/struktur-desa-contoh.png" alt="" />
     </div>
   </main>
 </template>
