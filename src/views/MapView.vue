2b3316b97ed074fec072125065f7a021514875db
ujangsuhandi1 <149266470+ujangsuhandi1@users.noreply.github.com>
2023-10-31 11:06:01 +0700

    Update MapView.vue

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -101,7 +101,7 @@ export default {
     async loadSomeGeoJson() {
       const nextIndex = this.layerData.length;
 
-      const response = await fetch("/ds_nyule.geojson");
+      const response = await fetch("/batas_desa_sukawening.json");
       const data = await response.json();
 
       console.log(data);
