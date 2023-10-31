792351d5d096e4dfa15e86d3fa30c5fd39ea4e6d
ujangsuhandi1 <149266470+ujangsuhandi1@users.noreply.github.com>
2023-10-31 10:55:50 +0700

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -15,7 +15,7 @@
       <iframe
         width="480"
         height="260"
-        src="https://www.youtube.com/embed/p_mIjIT5rU8?si=TF5ax68GJDf3S-UY"
+        src="https://www.youtube.com/embed/MSHNaok6hZ8?si=6PrhlT2UgEUeQkC-"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
